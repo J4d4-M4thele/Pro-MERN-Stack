f7377c64cf6d659c1bbd742c2e7ac6cba208a6da
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-09 11:45:20 +0200

    first commit

--- chapter05/public/App.js
+++ chapter05/public/App.js
@@ -44,12 +44,11 @@ var IssueFilter = /*#__PURE__*/function (_React$Component) {
     }
   }]);
   return IssueFilter;
-}(React.Component); //stateless component
+}(React.Component);
 function IssueRow(props) {
   var issue = props.issue;
   return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, issue.id), /*#__PURE__*/React.createElement("td", null, issue.status), /*#__PURE__*/React.createElement("td", null, issue.owner), /*#__PURE__*/React.createElement("td", null, issue.created.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.effort), /*#__PURE__*/React.createElement("td", null, issue.due ? issue.due.toDateString() : ''), /*#__PURE__*/React.createElement("td", null, issue.title));
 }
-//stateless component
 function IssueTable(props) {
   var issueRows = props.issues.map(function (issue) {
     return /*#__PURE__*/React.createElement(IssueRow, {
