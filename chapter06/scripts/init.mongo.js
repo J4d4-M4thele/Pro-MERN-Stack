09e34ceb12511e8a1e1b753e969589a70342000e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-10 13:40:46 +0200

    update

--- chapter06/scripts/init.mongo.js
+++ chapter06/scripts/init.mongo.js
@@ -12,3 +12,25 @@ scripts/init.mongo.js
 */
 
 db.issues.remove({});
+
+const issuesDB = [
+    {
+        id: 1, status: 'New', owner: 'Ravan', effort: 5,
+        created: new Date('2019-01-15'), due: undefined,
+        title: 'Error in console when clicking Add',
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+        created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+        title: 'Missing bottom border on panel',
+    },
+];
+
+db.issues.insertMany(issuesDB);
+const count = db.issues.count();
+print('Insert', count, 'issues');
+
+db.issues.createdIndex({ id: 1 }, { unique: true });
+db.issues.createdIndex({ status: 1 });
+db.issues.createdIndex({ owner: 1 });
+db.issues.createdIndex({ created: 1 });
