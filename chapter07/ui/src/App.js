d9497a6a77635cb632c4e15ef0cb4d8e6c825d1c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-14 14:04:50 +0200

    first commit

--- chapter07/ui/src/App.js
+++ chapter07/ui/src/App.js
@@ -83,7 +83,8 @@ class IssueAdd extends React.Component {
 
 async function graphQLFetch(query, variables = {}) {
   try {
-    const response = await fetch('http://localhost:3000/graphql', {
+    //links to backend localhost:3000
+    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify({ query, variables })
