55cec65de84e5e5ce3c3152df367d371a534195c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-16 15:34:20 +0200

    update

--- chapter09/ui/src/IssueList.js
+++ chapter09/ui/src/IssueList.js
@@ -1,68 +1,73 @@
 import React from 'react';
 import URLSearchParams from 'url-search-params';
+import { Route } from 'react-router-dom';
 
 import IssueFilter from './IssueFilter';
 import IssueTable from './IssueTable';
 import IssueAdd from './IssueAdd';
+import IssueDetail from './IssueDetail';
 import graphQLFetch from './graphQLFetch';
 
 export default class IssueList extends React.Component {
-    constructor() {
-      super();
-      this.state = { issues: [] };
-      this.createIssue = this.createIssue.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
 
-    componentDidUpdate(prevProps) {
-      const { location: {search: prevSearch} } = prevProps;
-      const { location: {search} } = this.props;
-      if(prevSearch !== search) {
-        this.loadData();
-      }
-    }
-
-    componentDidMount() {
+  componentDidUpdate(prevProps) {
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+    if (prevSearch !== search) {
       this.loadData();
     }
-  
-    async loadData() {
-      const query = `query issueList($status: StatusType) {
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    const query = `query issueList($status: StatusType) {
         issueList (status: $status) {
           id title status owner
           created effort due
         }
       }`;
-  
-      const data = await graphQLFetch(query, vars);
-      if (data) {
-        this.setState({ issues: data.issueList });
-      }
+
+    const data = await graphQLFetch(query, vars);
+    if (data) {
+      this.setState({ issues: data.issueList });
     }
-  
-    async createIssue(issue) {
-      const query = `mutation issueAdd($issue: IssueInputs!) {
+  }
+
+  async createIssue(issue) {
+    const query = `mutation issueAdd($issue: IssueInputs!) {
         issueAdd(issue: $issue) {
           id
         }
       }`;
-  
-      const data = await graphQLFetch(query, { issue });
-      if (data) {
-        this.loadData();
-      }
-    }
-  
-    render() {
-      const { issues } = this.state;
-      return (
-        <React.Fragment>
-          <h1>Issue Tracker</h1>
-          <IssueFilter />
-          <hr />
-          <IssueTable issues={issues} />
-          <hr />
-          <IssueAdd createIssue={this.createIssue} />
-        </React.Fragment>
-      );
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
     }
   }
+
+  render() {
+    const { issues } = this.state;
+    const { match } = this.props;
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+        <hr />
+        <Route path={`${match.path}/:id`} component={IssueDetail} />
+      </React.Fragment>
+    );
+  }
+}
