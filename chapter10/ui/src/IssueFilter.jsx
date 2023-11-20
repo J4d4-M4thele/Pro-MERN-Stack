3daccbb9e03d354fa94b5f6856e1d205f7be8387
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-20 14:54:59 +0200

    update

--- chapter10/ui/src/IssueFilter.jsx
+++ chapter10/ui/src/IssueFilter.jsx
@@ -6,13 +6,16 @@ class IssueFilter extends React.Component {
   constructor({ location: { search } }) {
     super();
     const params = new URLSearchParams(search);
-    //setting initial state
     this.state = {
       status: params.get('status') || '',
+      effortMin: params.get('effortMin') || '',
+      effortMax: params.get('effortMax') || '',
       changed: false,
     };
 
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.onChangeEffortMin = this.onChangeEffortMin.bind(this);
+    this.onChangeEffortMax = this.onChangeEffortMax.bind(this);
     this.applyFilter = this.applyFilter.bind(this);
     this.showOriginalFilter = this.showOriginalFilter.bind(this);
   }
@@ -26,18 +29,30 @@ class IssueFilter extends React.Component {
   }
 
   onChangeStatus(e) {
-    this.setState({
-      status: e.target.vallue,
-      changed: true,
-    });
+    this.setState({ status: e.target.value, changed: true });
+  }
+
+  onChangeEffortMin(e) {
+    const effortString = e.target.value;
+    if (effortString.match(/^\d*$/)) {
+      this.setState({ effortMin: e.target.value, changed: true });
+    }
+  }
+
+  onChangeEffortMax(e) {
+    const effortString = e.target.value;
+    if (effortString.match(/^\d*$/)) {
+      this.setState({ effortMax: e.target.value, changed: true });
+    }
   }
 
   showOriginalFilter() {
-    //displays default state set in constructor
     const { location: { search } } = this.props;
     const params = new URLSearchParams(search);
     this.setState({
       status: params.get('status') || '',
+      effortMin: params.get('effortMin') || '',
+      effortMax: params.get('effortMax') || '',
       changed: false,
     });
   }
@@ -45,11 +60,10 @@ class IssueFilter extends React.Component {
   applyFilter() {
     const { status, effortMin, effortMax } = this.state;
     const { history } = this.props;
-
     const params = new URLSearchParams();
     if (status) params.set('status', status);
     if (effortMin) params.set('effortMin', effortMin);
-    if (effortMax) params.set('status', effortMax);
+    if (effortMax) params.set('effortMax', effortMax);
 
     const search = params.toString() ? `?${params.toString()}` : '';
     history.push({ pathname: '/issues', search });
@@ -57,14 +71,12 @@ class IssueFilter extends React.Component {
 
   render() {
     //adding search parameters to the filter
-    const { location: { search } } = this.props;
-    const params = new URLSearchParams(search);
     const { status, changed } = this.state;
+    const { effortMin, effortMax } = this.state;
     return (
       <div>
         Status:
         {' '}
-        {/* value added to select */}
         <select value={status} onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
@@ -73,11 +85,24 @@ class IssueFilter extends React.Component {
           <option value="Closed">Closed</option>
         </select>
         {' '}
-        <button type='button' onClick={this.applyFilter}>
-          Apply
-        </button>
+        Effort between:
+        {' '}
+        <input
+          size={5}
+          value={effortMin}
+          onChange={this.onChangeEffortMin}
+        />
+        {' - '}
+        <input
+          size={5}
+          value={effortMax}
+          onChange={this.onChangeEffortMax}
+        />
+        {' '}
+        <button type="button" onClick={this.applyFilter}>Apply</button>
+        {' '}
         <button
-          type='button'
+          type="button"
           onClick={this.showOriginalFilter}
           disabled={!changed}
         >
