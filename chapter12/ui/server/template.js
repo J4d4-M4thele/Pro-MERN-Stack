1040e76f221da0530695be18087f5910fd319247
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-23 10:07:30 +0200

    update

--- chapter12/ui/server/template.js
+++ chapter12/ui/server/template.js
@@ -1,4 +1,4 @@
-function template(body) {
+export default function template(body) {
     return `<!DOCTYPE HTML>
     <html>
     <head>
@@ -21,4 +21,3 @@ function template(body) {
     </html>
     `;
 }
-module.exports = template;
