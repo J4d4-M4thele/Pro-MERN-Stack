9cabad083b422fd34971b1244f3aced0797d20cb
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-24 12:04:22 +0200

    first commit

---
+++ chapter13/ui/src/withToast.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Toast from "./Toast";
+
+export default function withToast(originalComponent) {
+    return class ToastWrapper extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                toastVisible: false, 
+                toastMessage: "", 
+                toastType: 'success',
+            };
+            this.showSuccess = this.showSuccess.bind(this);
+            this.showError = this.showError.bind(this);
+            this.dismissToast = this.dismissToast.bind(this);
+        }
+
+        showSuccess(message) {
+            this.setState({toastVisible: true, toastMessage: message, toastType: 'success'});
+        }
+
+        showError(message) {
+            this.setState({toastVisible: true, toastMessage: message, toastType: 'danger'});
+        }
+
+        dismissToast() {
+            this.setState({toastVisible: false});
+        }
+
+        render() {
+            const { toastType, toastVisible, toastMessage } = this.state;
+            return(
+                <React.Fragment>
+                    <originalComponent
+                    showError={this.showError}
+                    showSuccess={this.showSuccess}
+                    dismissToast={this.dismissToast}
+                    {...props}
+                    />
+                    <Toast
+                    bsStyle={toastType}
+                    showing={toastVisible}
+                    onDismiss={this.dismissToast}
+                    >
+                        {toastMessage}
+                    </Toast>
+                </React.Fragment>
+            )
+        }
+    }
+};
