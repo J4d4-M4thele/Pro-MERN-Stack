a9c0b7e5adc518096d1935559d526c6c8a4c7c54
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-11-24 12:36:00 +0200

    update

--- chapter13/ui/src/routes.js
+++ chapter13/ui/src/routes.js
@@ -4,9 +4,11 @@ import IssueEdit from './IssueEdit.jsx';
 import About from './About.jsx';
 import NotFound from './NotFound.jsx';
 
+import withToast from './withToast.jsx';
+
 const routes = [
   { path: '/issues/:id?', component: IssueList },
-  { path: '/edit/:id', component: IssueEdit },
+  { path: '/edit/:id', component: withToast(IssueEdit) },
   { path: '/report', component: IssueReport },
   { path: '/about', component: About },
   { path: '*', component: NotFound },
